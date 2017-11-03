7ebcdbb63a3a988209dabe9a2d4e32bf29c3a27f
Michał Gawron <michal@gawron.name>
2017-11-03 23:55:36 +0100

    Add Constring

---
+++ src/mulabs_avr/utility/constring.h
@@ -0,0 +1,91 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2017  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef MULABS_AVR__UTILITY__CONSTRING_H__INCLUDED
+#define MULABS_AVR__UTILITY__CONSTRING_H__INCLUDED
+
+namespace mulabs {
+namespace avr {
+
+template<class pChar>
+	class Constring
+	{
+	  public:
+		using Char = pChar;
+
+		/**
+		 * Thrown by operator[].
+		 */
+		class OutOfRange
+		{ };
+
+	  public:
+		// Ctor
+		template<size_t N>
+			constexpr
+			Constring (Char const (&data)[N]);
+
+		constexpr Char const*
+		data() const;
+
+		constexpr size_t
+		size() const;
+
+		constexpr Char
+		operator[] (size_t n) const;
+
+	  private:
+		Char const*	_data;
+		size_t		_size;
+	};
+
+
+template<class C>
+	template<size_t N>
+		constexpr
+		Constring<C>::Constring (Char const (&data)[N]):
+			_data (data),
+			_size (N - 1) // -1 is not to count the "\0" from the string literal
+		{ }
+
+
+template<class C>
+	constexpr typename Constring<C>::Char const*
+	Constring<C>::data() const
+	{
+		return _data;
+	}
+
+
+template<class C>
+	constexpr size_t
+	Constring<C>::size() const
+	{
+		return _size;
+	}
+
+
+template<class C>
+	constexpr typename Constring<C>::Char
+	Constring<C>::operator[] (size_t n) const
+	{
+		return n < _size
+			? _data[n]
+			: throw OutOfRange();
+	}
+
+} // namespace avr
+} // namespace mulabs
+
+#endif
+
